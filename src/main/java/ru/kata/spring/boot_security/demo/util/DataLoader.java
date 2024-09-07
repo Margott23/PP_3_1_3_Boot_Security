fae1dc0f239bbec44fe63efb26dda620445032b9
Margott23 <margott23@mail.ru>
2024-09-07 17:52:34 +0300

    checked

--- src/main/java/ru/kata/spring/boot_security/demo/util/DataLoader.java
+++ src/main/java/ru/kata/spring/boot_security/demo/util/DataLoader.java
@@ -4,30 +4,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.dao.RoleDAOImpl;
+import ru.kata.spring.boot_security.demo.dao.UserDAOImpl;
 import ru.kata.spring.boot_security.demo.models.Role;
 import ru.kata.spring.boot_security.demo.models.User;
-import ru.kata.spring.boot_security.demo.repositories.RoleRepository;
-import ru.kata.spring.boot_security.demo.repositories.UserRepository;
 
 import java.util.List;
 
 @Component
 public class DataLoader implements CommandLineRunner {
 
-    private final RoleRepository roleRepository;
-    private final UserRepository userRepository;
+    private final RoleDAOImpl roleRepository;
+    private final UserDAOImpl userRepository;
     private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public DataLoader(RoleRepository roleRepository, UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public DataLoader(RoleDAOImpl roleRepository, UserDAOImpl userRepository, PasswordEncoder passwordEncoder) {
         this.roleRepository = roleRepository;
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
     @Override
+    @Transactional
     public void run(String... args) {
-        if (userRepository.count() == 0) {
+        if (userRepository.findAll().isEmpty()) {
             Role userRole = new Role("ROLE_USER");
             Role adminRole = new Role("ROLE_ADMIN");
             User user = new User(
